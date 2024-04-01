6b9b6e1dfa8e91b7083fcce372d5209a6b128ca0
mihawk <anill.stha23.as@gmail.com>
2024-04-01 08:35:23 +0545

    show loading skeleton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,11 +10,11 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <>
-      <Card borderRadius={10} overflow="hidden">
+      <Card width='300px'   borderRadius={10} overflow="hidden">
         <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
           <Heading fontSize={20}>{game.name}</Heading>
-          <HStack justifyContent={"space-between"}>
+          <HStack justifyContent='space-between'>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
